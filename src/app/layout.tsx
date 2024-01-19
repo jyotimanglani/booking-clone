ea5406553fb84f3dc9cfb4bad6da2b35b7b9bc6d
Jyoti Manglani <manglanijyoti62@gmail.com>
2024-01-20 00:55:20 +0530

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Booking.com clone",
@@ -14,8 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        
+        <Header/>
+
         {children}</body>
     </html>
   );
